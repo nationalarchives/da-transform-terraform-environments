9cbef7e70910b04daccfb7774fd447bf74479dee
rahmedzaizi <rahmed@zaizi.com>
2022-02-10 15:58:04 +0000

    merged with develop

--- common/iam.tf
+++ common/iam.tf
@@ -30,34 +30,6 @@ resource "aws_iam_role_policy_attachment" "terraform" {
   policy_arn = data.aws_iam_policy.managed_admin.arn
 }
 
-## Cross-account role for admin
-data "aws_caller_identity" "users" {
-  provider = aws.users
-}
-
-data "aws_iam_policy_document" "zaizi_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.users.id}:root"]
-    }
-  }
-}
-
-resource "aws_iam_role" "cross_account_admin" {
-  name               = "Zaizi_Admin_Role"
-  path               = "/zaizi/"
-  assume_role_policy = data.aws_iam_policy_document.zaizi_assume_role_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "cross-account-admin" {
-  role       = aws_iam_role.cross_account_admin.name
-  policy_arn = data.aws_iam_policy.managed_admin.arn
-
-}
-
 # Pipeline policies and permissions
 
 data "aws_iam_policy_document" "codepipeline-assume-role-policy" {
