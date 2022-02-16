af78832eb185328fbe293a463b1d807ac3f938b8
rahmedzaizi <rahmed@zaizi.com>
2022-02-16 11:32:03 +0000

    modified script to create terratest logs

--- terratest.sh
+++ terratest.sh
@@ -12,7 +12,10 @@ cd da-transform-terraform-modules
 ls
 git branch
 git checkout test_branch
-cd ../src
-bash <(curl -s https://raw.githubusercontent.com/nationalarchives/da-transform-terraform-modules/test_branch/terratest.sh)
+./terratest.sh
+# cd ../src
+# bash <(curl -s https://raw.githubusercontent.com/nationalarchives/da-transform-terraform-modules/test_branch/terratest.sh)
 
-cat ../da-transform-terraform-modules/test/reports/hello_world_test_output.log
+cat test/reports/hello_world_test_output.log
+
+exit
