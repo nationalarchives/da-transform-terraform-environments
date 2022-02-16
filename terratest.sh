0e7767be7d1c67e1f1608ec085520b6f219febe1
rahmedzaizi <rahmed@zaizi.com>
2022-02-16 02:58:31 +0000

    terratest script added to test remote modules

--- terratest.sh
+++ terratest.sh
@@ -7,7 +7,9 @@ pwd
 git init
 git clone https://github.com/nationalarchives/da-transform-terraform-modules.git
 pwd
-
+cat da-transform-terraform-modules/hello_world/test/test.sh
+cd da-transform-terraform-modules
+./terratest.sh
 ls
-cd src
+cd ../src
 bash <(curl -s https://raw.githubusercontent.com/nationalarchives/da-transform-terraform-modules/test_branch/terratest.sh)
