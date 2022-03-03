f4c62ca55eda99f90dd59c63fb8ad0ef09d6b325
rahmedzaizi <rahmed@zaizi.com>
2022-03-03 08:26:59 +0000

    removed sfn temp

--- deployments/main.tf
+++ deployments/main.tf
@@ -1,11 +1,11 @@
-module "pipeline_step_function" {
-  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=test_branch//step_function"
-  env = var.environment_name
-}
+# module "pipeline_step_function" {
+#   source = "github.com/nationalarchives/da-transform-terraform-modules?ref=test_branch//step_function"
+#   env = var.environment_name
+# }
 
 module "tdr_sqs_in_queue" {
   source = "github.com/nationalarchives/da-transform-terraform-modules?ref=test_branch//sqs"
   env = var.environment_name
   tdr_role_arn = var.tdr_role_arn
-  sfn_arn = module.pipeline_step_function.sfn_state_machine_arn
+  sfn_arn = "arn:aws:states:eu-west-2:882876621099:stateMachine:dev-te-state-machine"
 }
