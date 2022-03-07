012fc69116593abba710b41b8b64a1cf0b35f487
rahmedzaizi <rahmed@zaizi.com>
2022-03-07 17:10:34 +0000

    both checksum completed

--- deployments/main.tf
+++ deployments/main.tf
@@ -1,6 +1,8 @@
 module "pipeline_step_function" {
   source = "github.com/nationalarchives/da-transform-terraform-modules?ref=test_branch//step_function"
   env = var.environment_name
+  tdr_sqs_queue_endpoint = var.tdr_sqs_queue_endpoint
+  tdr_sqs_queue_arn = var.tdr_sqs_queue_arn
 }
 
 module "tdr_sqs_in_queue" {
--- deployments/variables.tf
+++ deployments/variables.tf
@@ -17,3 +17,13 @@ variable "tdr_role_arn" {
   description = "role ARN for TDR to submit to SQS queues"
   type = string
 }
+
+variable "tdr_sqs_queue_endpoint" {
+  description = "Endpoint of the TDR SQS Queue for the retry message"
+  type = string
+}
+
+variable "tdr_sqs_queue_arn" {
+  description = "ARN of the TDR SQS Queue for the retry message"
+  type = string
+}
