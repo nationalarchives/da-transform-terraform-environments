6004a3613da57d91edcf9024378fa8b9440ac163
rahmedzaizi <rahmed@zaizi.com>
2022-04-13 13:12:54 +0100

    tre-slack-alerts ecr repo added

--- common/ecr.tf
+++ common/ecr.tf
@@ -6,3 +6,13 @@ resource "aws_ecr_repository" "tre_parser" {
     scan_on_push = true
   }
 }
+
+
+resource "aws_ecr_repository" "tre_slack_alerts" {
+  name = "lambda_functions/tre-slack-alerts"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
