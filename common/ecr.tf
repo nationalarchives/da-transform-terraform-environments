78a72ef17ab2e18a23618ed96f3d823cebe60f3f
rahmedzaizi <rahmed@zaizi.com>
2022-04-13 01:16:35 +0100

    parser ecr repo name changed

--- common/ecr.tf
+++ common/ecr.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "tna_parser" {
-  name = "lambda_functions/te-text-parser"
+resource "aws_ecr_repository" "tre_parser" {
+  name = "lambda_functions/tre-judgment-parser"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
