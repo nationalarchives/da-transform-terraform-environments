454e7e99ffda91427c5d47507719f0189627dda5
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-04-25 12:36:13 +0100

    Fix issue from incorrect github UI operation

--- deployments/main.tf
+++ deployments/main.tf
@@ -1,5 +1,5 @@
 module "pipeline_step_function" {
-  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=staging//step_function"
+  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=int//step_function"
   env = var.environment_name
   prefix = var.prefix
   tdr_sqs_queue_endpoint = var.tdr_sqs_queue_endpoint
@@ -16,7 +16,7 @@ module "pipeline_step_function" {
 }
 
 module "tdr_sqs_in_queue" {
-  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=staging//sqs"
+  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=int//sqs"
   env = var.environment_name
   prefix = var.prefix
   tdr_role_arn = var.tdr_role_arn
