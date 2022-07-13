8d8590a872bddcb2b4318afd462300dddde42a47
rahmedzaizi <rahmed@zaizi.com>
2022-07-13 11:17:47 +0100

    merging to test

--- deployments/main.tf
+++ deployments/main.tf
@@ -1,5 +1,5 @@
 module "pipeline_step_function" {
-  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=dev//step_function"
+  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=test//step_function"
   env = var.environment_name
   prefix = var.prefix
   tdr_sqs_queue_endpoint = var.tdr_sqs_queue_endpoint
@@ -17,7 +17,7 @@ module "pipeline_step_function" {
 }
 
 module "tdr_sqs_in_queue" {
-  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=dev//sqs"
+  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=test//sqs"
   env = var.environment_name
   prefix = var.prefix
   tdr_role_arn = var.tdr_role_arn
@@ -30,7 +30,7 @@ module "tdr_sqs_in_queue" {
 # Common
 
 module "common" {
-  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=dev//common"
+  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=test//common"
   env    = var.environment_name
   prefix = var.prefix
   account_id = data.aws_caller_identity.aws.account_id
@@ -49,7 +49,7 @@ module "common" {
 # Receive and process bag
 
 module "receive_and_process_bag" {
-  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=dev//step_functions/receive_and_process_bag"
+  source = "github.com/nationalarchives/da-transform-terraform-modules?ref=test//step_functions/receive_and_process_bag"
   env = var.environment_name
   prefix = var.prefix
   account_id = data.aws_caller_identity.aws.account_id
