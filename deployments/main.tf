7b34507aaee0c5a7673b02160205425a41a20058
rahmedzaizi <rahmed@zaizi.com>
2022-07-13 12:07:37 +0100

    adding new variables for tdr retry queue

--- deployments/main.tf
+++ deployments/main.tf
@@ -58,6 +58,6 @@ module "receive_and_process_bag" {
   rapb_version = var.rapb_version
   common_tre_slack_alerts_topic_arn = module.common.common_tre_slack_alerts_topic_arn
   common_tre_in_sns_topic_arn = module.common.common_tre_in_sns_topic_arn
-  tdr_sqs_retry_url = var.tdr_sqs_queue_endpoint
-  tdr_sqs_retry_arn = var.tdr_sqs_queue_arn
+  tdr_sqs_retry_url = var.tdr_sqs_retry_url
+  tdr_sqs_retry_arn = var.tdr_sqs_retry_arn
 }
