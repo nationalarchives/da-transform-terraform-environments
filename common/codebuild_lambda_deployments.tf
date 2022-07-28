4b5f770d0dc2470cb62df69b0197561a3e4e0369
rahmedzaizi <rahmed@zaizi.com>
2022-07-28 17:40:26 +0100

    stages added to deploy lambda images

--- common/codebuild_lambda_deployments.tf
+++ common/codebuild_lambda_deployments.tf
@@ -55,6 +55,11 @@ resource "aws_codebuild_project" "tre_update_dev_tfvars" {
       value = "tre-lambda-deploy-sf-tfvar-keys"
       type = "PARAMETER_STORE"
     }
+
+    environment_variable {
+      name = "ENV_TF_VARS"
+      value = "dte-96-tfvars"
+    }
   }
 
   logs_config {
@@ -65,12 +70,176 @@ resource "aws_codebuild_project" "tre_update_dev_tfvars" {
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = file("files/tre_update_dev_tfvars.yaml")
+    buildspec = file("files/tre_update_env_tfvars.yaml")
   }
 }
 
-resource "aws_codebuild_project" "run_pipeline" {
-  name = "run_pipeline"
+resource "aws_codebuild_project" "tre_update_test_tfvars" {
+  name = "tre_update_test_tfvars"
+  description = "Codebuild for updating lambda verions in test"
+  build_timeout = "10"
+  service_role = aws_iam_role.mgmt_terraform.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name = "SF_TF_VAR_KEY_LIST"
+      value = "tre-lambda-deploy-sf-tfvar-keys"
+      type = "PARAMETER_STORE"
+    }
+
+    environment_variable {
+      name = "ENV_TF_VARS"
+      value = "dte-96-tfvars"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "da-transform-lambda-deployment-pipeline-logs"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("files/tre_update_env_tfvars.yaml")
+  }
+}
+
+resource "aws_codebuild_project" "tre_update_int_tfvars" {
+  name = "tre_update_int_tfvars"
+  description = "Codebuild for updating lambda verions in int"
+  build_timeout = "10"
+  service_role = aws_iam_role.mgmt_terraform.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name = "SF_TF_VAR_KEY_LIST"
+      value = "tre-lambda-deploy-sf-tfvar-keys"
+      type = "PARAMETER_STORE"
+    }
+
+    environment_variable {
+      name = "ENV_TF_VARS"
+      value = "dte-96-tfvars"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "da-transform-lambda-deployment-pipeline-logs"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("files/tre_update_env_tfvars.yaml")
+  }
+}
+
+resource "aws_codebuild_project" "tre_update_staging_tfvars" {
+  name = "tre_update_staging_tfvars"
+  description = "Codebuild for updating lambda verions in staging"
+  build_timeout = "10"
+  service_role = aws_iam_role.mgmt_terraform.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name = "SF_TF_VAR_KEY_LIST"
+      value = "tre-lambda-deploy-sf-tfvar-keys"
+      type = "PARAMETER_STORE"
+    }
+
+    environment_variable {
+      name = "ENV_TF_VARS"
+      value = "dte-96-tfvars"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "da-transform-lambda-deployment-pipeline-logs"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("files/tre_update_env_tfvars.yaml")
+  }
+}
+
+resource "aws_codebuild_project" "tre_update_prod_tfvars" {
+  name = "tre_update_prod_tfvars"
+  description = "Codebuild for updating lambda verions in prod"
+  build_timeout = "10"
+  service_role = aws_iam_role.mgmt_terraform.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name = "SF_TF_VAR_KEY_LIST"
+      value = "tre-lambda-deploy-sf-tfvar-keys"
+      type = "PARAMETER_STORE"
+    }
+
+    environment_variable {
+      name = "ENV_TF_VARS"
+      value = "dte-96-tfvars"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "da-transform-lambda-deployment-pipeline-logs"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("files/tre_update_env_tfvars.yaml")
+  }
+}
+
+resource "aws_codebuild_project" "run_pipeline_dev" {
+  name = "run_pipeline_dev"
   description = "Codebuild to trigger terraform-dev pipeline"
   build_timeout = "20"
   service_role = aws_iam_role.mgmt_terraform.arn
@@ -85,6 +254,11 @@ resource "aws_codebuild_project" "run_pipeline" {
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
+
+    environment_variable {
+      name = "PIPELINE_NAME"
+      value = "terraform-dev"
+    }
   }
 
   logs_config {
@@ -95,6 +269,146 @@ resource "aws_codebuild_project" "run_pipeline" {
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = file("files/run_pipeline_dev.yaml")
+    buildspec = file("files/run_pipeline.yaml")
+  }
+}
+
+resource "aws_codebuild_project" "run_pipeline_test" {
+  name = "run_pipeline_test"
+  description = "Codebuild to trigger terraform-test pipeline"
+  build_timeout = "20"
+  service_role = aws_iam_role.mgmt_terraform.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name = "PIPELINE_NAME"
+      value = "terraform-test"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "da-transform-lambda-deployment-pipeline-logs"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("files/run_pipeline.yaml")
+  }
+}
+
+resource "aws_codebuild_project" "run_pipeline_int" {
+  name = "run_pipeline_int"
+  description = "Codebuild to trigger terraform-int pipeline"
+  build_timeout = "20"
+  service_role = aws_iam_role.mgmt_terraform.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name = "PIPELINE_NAME"
+      value = "terraform-dev"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "da-transform-lambda-deployment-pipeline-logs"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("files/run_pipeline.yaml")
+  }
+}
+
+resource "aws_codebuild_project" "run_pipeline_staging" {
+  name = "run_pipeline_staging"
+  description = "Codebuild to trigger terraform-staging pipeline"
+  build_timeout = "20"
+  service_role = aws_iam_role.mgmt_terraform.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name = "PIPELINE_NAME"
+      value = "terraform-dev"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "da-transform-lambda-deployment-pipeline-logs"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("files/run_pipeline.yaml")
+  }
+}
+
+resource "aws_codebuild_project" "run_pipeline_prod" {
+  name = "run_pipeline_prod"
+  description = "Codebuild to trigger terraform-prod pipeline"
+  build_timeout = "20"
+  service_role = aws_iam_role.mgmt_terraform.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name = "PIPELINE_NAME"
+      value = "terraform-dev"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "da-transform-lambda-deployment-pipeline-logs"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("files/run_pipeline.yaml")
   }
 }
