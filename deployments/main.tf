ec112db7923e21f4adf34ec5ba86a2d9df2ba8d7
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-08-11 15:18:44 +0100

    Remove trailing whitespace

--- deployments/main.tf
+++ deployments/main.tf
@@ -46,7 +46,7 @@ module "common" {
   slack_username = var.slack_username
   tre_rapb_in_queue_arn = module.receive_and_process_bag.tre_rapb_in_queue_arn
   tre_in_publishers = var.tre_in_publishers
-  tre_in_subscribers = var.tre_in_subscribers  
+  tre_in_subscribers = var.tre_in_subscribers
   tre_internal_publishers = [
     module.receive_and_process_bag.receive_and_process_bag_role_arn
   ]
@@ -54,7 +54,7 @@ module "common" {
   tre_out_publishers = [
     module.receive_and_process_bag.receive_and_process_bag_role_arn
   ]
-  tre_out_subscribers = var.tre_out_subscribers 
+  tre_out_subscribers = var.tre_out_subscribers
 }
 
 # Receive and process bag
