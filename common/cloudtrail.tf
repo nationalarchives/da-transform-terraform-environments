cce5fc5ea7947ad04ed2f5e9de83ed4ab400071e
Giuseppe Malanga <gmalanga@zaizi.com>
2022-08-25 21:32:00 +0100

    Added resources aws_s3_bucket_acl and aws_s3_bucket_versioning

--- common/cloudtrail.tf
+++ common/cloudtrail.tf
@@ -14,12 +14,20 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
   }
 }
 
+resource "aws_s3_bucket_acl" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_versioning" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "${local.bucket_name}-logs"
-
-  versioning {
-    enabled = true
-  }
 }
 
 resource "aws_cloudtrail" "cloudtrail" {
