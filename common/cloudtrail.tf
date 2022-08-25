a5c2077cf0d7449285f8f38c4def613c1f56f28e
Giuseppe Malanga <gmalanga@zaizi.com>
2022-08-25 21:48:16 +0100

    Updated acl for aws_s3_bucket_acl

--- common/cloudtrail.tf
+++ common/cloudtrail.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
 
 resource "aws_s3_bucket_acl" "log_bucket" {
   bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "log_bucket" {
